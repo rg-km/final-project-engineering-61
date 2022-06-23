ef47427e830611e6d35d9e3a5a377e4d706a4719
yusuf rismanda <1910631170109@student.unsika.ac.id>
2022-06-23 09:54:18 +0700

    create user page, kategory materi, materi page.(api still using dummy data from json-server)

--- Frontend/kuas/src/components/pages/user/MateriUser.js
+++ Frontend/kuas/src/components/pages/user/MateriUser.js
@@ -1,12 +1,30 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 import NavUser from '../../NavUser'
+import Whatsapp from '../../Whatsapp';
+import './materi.css'
+import smp from '../../img/smp.jpg';
+import sma from '../../img/sma.jpg';
 
 export default function MateriUser() {
   return (
     <>
     <NavUser/>
-        <div>MateriUser</div>
-    
+    <main className="main-menu">
+      <Link to="/user/materi-belajar/smp">
+        <div>
+          <h1>SMP</h1>
+          <img src={smp} width={350} alt="Smp Thumbnail" />
+        </div>
+      </Link>
+      <Link to="/user/materi-belajar/sma">
+        <div>
+          <h1>SMA</h1>
+          <img src={sma} width={350} alt="Sma Thumbnail" />
+        </div>
+      </Link>
+    </main>
+    <Whatsapp/>
     </>
   )
 }
